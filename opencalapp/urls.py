2a23e03b6edc8649bac77781d4764f7b8c90acc7
Christopher Lackowski <christopherlackowski@yahoo.com>
2024-08-08 10:49:52 -0400

    Init footer pages and improved footer navigation

--- opencalapp/urls.py
+++ opencalapp/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('register/', views.register, name='register'),
     path('footer/', views.show_footer, name='footer'),
-    path('mycalendars/', views.show_mycalendars, name='mycalendars')
+    path('mycalendars/', views.show_mycalendars, name='mycalendars'),
+    path('about/', views.about, name='about'),
+    path('contactus/', views.contact_us, name='contact_us'),
+    path('helpcenter/', views.help_center, name='help_center'),
+    path('forgotpassword/', views.forgot_password, name='forgot_password'),
 ]
